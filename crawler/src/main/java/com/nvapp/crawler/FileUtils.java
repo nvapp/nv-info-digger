b0c6c2502c89baef7167c6d40d4cecb087e825ad
lexloo <lexloo@126.com>
2015-01-13 22:23:28 +0800

    可以保存文件

---
+++ crawler/src/main/java/com/nvapp/crawler/FileUtils.java
@@ -0,0 +1,35 @@
+/**
+ * NextVisual
+ */
+/**
+ * NextVisual
+ */
+package com.nvapp.crawler;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.UUID;
+
+/**
+ * 
+ * 
+ * @version 1.0
+ * @author <a herf="lexloo@gmail.com">lexloo</a>
+ * @since 销售宝 2.0
+ * 
+ *        <pre>
+ * 历史：
+ *      建立: 2015-1-13 lexloo
+ * </pre>
+ */
+public class FileUtils {
+    public static final void saveToFile(String content) {
+        File file = new File("d:/wett/" + UUID.randomUUID().toString() + ".txt");
+        try (FileWriter fr = new FileWriter(file)) {
+            fr.write(content);
+            fr.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- crawler/src/main/java/com/nvapp/crawler/TextCrawler.java
+++ crawler/src/main/java/com/nvapp/crawler/TextCrawler.java
@@ -7,7 +7,12 @@
 package com.nvapp.crawler;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
@@ -41,7 +46,6 @@ public class TextCrawler extends WebCrawler {
     @Override
     public boolean shouldVisit(WebURL url) {
         String href = url.getURL().toLowerCase();
-        System.out.println(href);
         if (FILTERS.matcher(href).matches()) {
             return false;
         }
@@ -54,43 +58,23 @@ public class TextCrawler extends WebCrawler {
      */
     @Override
     public void visit(Page page) {
-        String url = page.getWebURL().getURL();
-        System.out.println("开始处理:" + url);
         if (page.getParseData() instanceof HtmlParseData) {
             HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
             String html = htmlParseData.getHtml();
 
-            System.out.println(html);
-            // Document doc = Jsoup.parse(html);
-            // String brand = doc.select("div.choose_item").first().text();
-            //
-            // Elements contents = doc.select("div.list_content");
-            //
-            // if (contents.size() == 20 && !url.contains("index=")) {
-            // return;
-            // } else {
-            // System.out.println("URL: " + url);
-            // }
-            //
-            // for (Element c : contents) {
-            // Element info = c.select(".list_content_carInfo").first();
-            // String title = info.select("h1").first().text();
-            //
-            // Elements prices = info.select(".list_content_price div");
-            // String newPrice = prices.get(0).text();
-            // String oldPrice = prices.get(1).text();
-            //
-            // Elements others = info.select(".list_content_other div");
-            // String mileage = others.get(0).select("ins").first().text();
-            // String age = others.get(1).select("ins").first().text();
-            //
-            // String stage = "unknown";
-            // if (c.select("i.car_tag_zhijian").size() != 0) {
-            // stage = c.select("i.car_tag_zhijian").text();
-            // } else if (c.select("i.car_tag_yushou").size() != 0) {
-            // stage = "presell";
-            // }
-            // }
+            Document doc = Jsoup.parse(html);
+            Iterator<Element> it = doc.select("div span").iterator();
+            while (it.hasNext()) {
+                Element el = it.next();
+
+                if (el.hasText()) {
+                    String content = el.text();
+
+                    if (content.contains("地震") || content.contains("厦门")) {
+                        FileUtils.saveToFile(content);
+                    }
+                }
+            }
         }
     }
 }
